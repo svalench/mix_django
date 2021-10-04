0d20f97fb557693cdfac48a22b575a8755876582
svalench <chitsalex@gmail.com>
2021-10-04 11:40:54 +0300

    pg connector

--- mix_django/settings.py
+++ mix_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4a%m+5z9x_z!4z6e3vpbgd#&nrx=o8v&r1xdc=(sn%+x^#cjxj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api.mixenerdgy.by']
+ALLOWED_HOSTS = ['api.mixenerdgy.by', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -90,13 +90,25 @@ AUTH_USER_MODEL = 'user.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+print(os.environ)
+if os.environ.get('prod'):
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('name_django_db'),
+        'USER': os.environ.get('user_django_db'),
+        'PASSWORD': os.environ.get('password_django_db'),
+        'HOST': 'localhost',
+        'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
