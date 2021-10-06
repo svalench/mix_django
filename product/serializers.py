3f24f3737553ad395729decf4314a6142db9785f
svalench <chitsalex@gmail.com>
2021-10-07 00:23:40 +0300

    img

--- product/serializers.py
+++ product/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 
-from product.models import Product, CardProduct, Characteristics, CharacteristicValue
+from product.models import Product, CardProduct, Characteristics, CharacteristicValue, ProductsImages
 
+
+class CardImagesSerializer(serializers.ModelSerializer):
+    """сериализация модели Characteristics """
+    class Meta:
+        model = ProductsImages
+        permission_classes = (IsAuthenticated,)
+        fields = '__all__'
 
 class CharacteristicSerializer(serializers.ModelSerializer):
     """сериализация модели Characteristics """
@@ -24,6 +31,14 @@ class CharacteristicValueSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     """сериализация модели Product """
     characteristics_norm = CharacteristicValueSerializer(source='characteristics.all', read_only=True, many=True)
+    images = CardImagesSerializer(source='parent.images.all', read_only=True, many=True)
+    img = serializers.SerializerMethodField()
+
+    def get_img(self, obj):
+        if obj.parent:
+            return obj.parent.img
+        else:
+            return None
 
     class Meta:
         model = Product
