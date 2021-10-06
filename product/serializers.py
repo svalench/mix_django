9aa94dc6469c6a6ebd13f7be9f8352663bfcdb85
svalench <chitsalex@gmail.com>
2021-10-07 00:06:21 +0300

    characterisitics

--- product/serializers.py
+++ product/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 
-from product.models import Product, CardProduct
+from product.models import Product, CardProduct, Characteristics, CharacteristicValue
+
+
+class CharacteristicSerializer(serializers.ModelSerializer):
+    """сериализация модели Characteristics """
+    class Meta:
+        model = Characteristics
+        permission_classes = (IsAuthenticated,)
+        fields = '__all__'
+
+
+class CharacteristicValueSerializer(serializers.ModelSerializer):
+    """сериализация модели Characteristics """
+    parent = CharacteristicSerializer(source='parent', read_only=True, many=True)
+    class Meta:
+        model = CharacteristicValue
+        permission_classes = (IsAuthenticated,)
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
     """сериализация модели Product """
+    characteristics_norm = CharacteristicValueSerializer(source='characteristics.all', read_only=True, many=True)
+
     class Meta:
         model = Product
         permission_classes = (IsAuthenticated,)
