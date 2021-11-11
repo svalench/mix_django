ec50a14582e329ae80a0219fd0ff37858364fa73
svalench <chitsalex@gmail.com>
2021-11-11 23:30:05 +0300

    fix mail

--- mix_django/settings.py
+++ mix_django/settings.py
@@ -217,7 +217,7 @@ EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'mixenerdgy.by@yandex.by'
-EMAIL_HOST_PASSWORD = '8940113Wert'
+EMAIL_HOST_PASSWORD = '123123123'
 
 DOMAIN = 'mixenerdgy.by'
 MANAGER_EMAIL = 'mixenerdgy@mail.ru'
