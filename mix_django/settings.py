b4c41fd43684fc3bb221cd6ad7e9ee81d04e76b3
svalench <chitsalex@gmail.com>
2021-11-11 22:53:29 +0300

    fix mail

--- mix_django/settings.py
+++ mix_django/settings.py
@@ -213,7 +213,7 @@ GRAPPELLI_ADMIN_TITLE = 'MixEnergy'
 #==========================EMAIL SETTINGS==========================================================/
 
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+# EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'mixenerdgy.by@yandex.by'
