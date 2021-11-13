81006c3284fa291dbe8ba1dfd8c0501d6aeb90ea
svalench <chitsalex@gmail.com>
2021-11-13 21:49:56 +0300

    char for show

--- product/serializers.py
+++ product/serializers.py
@@ -104,7 +104,7 @@ class ProductSerializer(serializers.ModelSerializer):
         return obj.parent.characteristic_for_show.charac_value.all().first().units.name
 
     def get_search_filter(self, obj):
-        return [x.characteristics.filter(parent__id=obj.parent.characteristic_for_show.id).first().values()
+        return [x.characteristics.filter(parent__id=obj.parent.characteristic_for_show.id).first()
                 for x in obj.parent.child.all()]
 
 
